c159af4a969e91ce4299231289803650cfcf88c2
Ricardo Guerrero <richi_gro@hotmail.com>
2025-04-11 10:09:48 -0500

    Now able to move character with sprite walking animation

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,5 +1,7 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable {
     final int originalTileSize = 16; // 16X16 tiles
     final int scale = 3; // Scale things by 3 to accommodate modern resolution monitors
 
-    final int tileSize = originalTileSize * scale; // This will be the actual size of the tiles (48X48)
+    public final int tileSize = originalTileSize * scale; // This will be the actual size of the tiles (48X48)
     final int maxScreenCol = 16;
     final int maxScreenRow = 12; // This will give us an aspect ratio of 4/3
     final int screenWidth = tileSize * maxScreenCol; // 48 X 16 = 768px
@@ -16,14 +18,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-
-    // Set player size
-    final int playerWidth = tileSize;
-    final int playerHeight = tileSize;
-    // Set player default position
-    int playerX = 100;
-    int playerY = 100;
-    int playerSpeed = 4;
+    Player player = new Player(this, keyH);
 
     // FPS frame per second
     int FPS = 60;
@@ -43,7 +38,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        double drawInterval = (double) 1000000000 /FPS; // 0.01666 seconds to be able to draw 60 frame in a second
+        double drawInterval = (double) 1000000000 / FPS; // 0.01666 seconds to be able to draw 60 frame in a second
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
@@ -74,24 +69,15 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void update() {
-        if(keyH.upPressed) {
-            playerY -= playerSpeed;
-        } else if(keyH.downPressed) {
-            playerY += playerSpeed;
-        } else if( keyH.rightPressed) {
-            playerX += playerSpeed;
-        } else if(keyH.leftPressed) {
-            playerX -= playerSpeed;
-        }
+        player.update();
     }
+
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g); // the Parent class Jpanel has method called paintComponent
-
         Graphics2D g2 = (Graphics2D)g; // Ready to draw 2d graphics
 
-        g2.setColor(Color.white); // the color to paint with
-        g2.fillRect(playerX, playerY, playerWidth, playerHeight);
+        player.draw(g2);
 
         g2.dispose();
     }
